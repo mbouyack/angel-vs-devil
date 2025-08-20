e5e3df4d3d2211551a66b4b8885058f03b517248
Matthew Bouyack <matthew@bouyack.tech>
2025-08-20 10:09:47 -0400

    Rename 'step' to 'sprint_partial'
    'step' was too similar in name to the 'steps' defined in Defs.lean

--- AngelDevil/Runner.lean
+++ AngelDevil/Runner.lean
@@ -2,6 +2,8 @@ import Mathlib.Data.Fin.Basic
 import Mathlib.Data.List.Basic
 import AngelDevil.Defs
 import AngelDevil.Basic
+import AngelDevil.Append
+import AngelDevil.Subjourney
 
 set_option linter.style.longLine false
 
@@ -72,31 +74,27 @@ def next_step (eaten : List (Int × Int)) (rs : RunState) : RunState :=
   if ¬blocked eaten (loc (move_forward rs)) then move_forward rs else
   turn_right rs
 
--- TODO: Consider using a different name than 'step' since "steps" already
--- refers to the cells in a journey
-/- Add one step to the run.
-   If the step would exceed the maximum allowed steps per sprint, or is not "close"
-   to the starting point of the sprint, then instead end the sprint.
-   If the sprint doesn't end, then try taking the next step.
+/- Attempts to add 'rs' to the current partial sprint.
+   If it succeeds, repeat with the cell indicated by 'next_step'.
 -/
-def step (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) : List RunState :=
+def sprint_partial (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) : List RunState :=
   if hterm : 2 * p < ns ∨ ¬close p start (loc rs) then [] else
-  rs :: (step p (ns + 1) start eaten (next_step eaten rs))
+  rs :: (sprint_partial p (ns + 1) start eaten (next_step eaten rs))
 termination_by 2 * p + 1 - ns
 decreasing_by
   push_neg at hterm
   rw [add_comm ns, Nat.sub_add_eq, Nat.add_one_sub_one, Nat.sub_add_comm hterm.1]
   exact Nat.lt_add_one_of_le (le_refl _)
 
--- If the list constructed by 'step' is not empty, the first element of the list will be 'rs'
-lemma step_getElem_zero_of_nonnil (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
-  (hnnil : step p ns start eaten rs ≠ []) → (step p ns start eaten rs)[0]'(List.length_pos_iff.mpr hnnil) = rs := by
+-- If the list constructed by 'sprint_partial' is not empty, the first element of the list will be 'rs'
+lemma sprint_partial_getElem_zero_of_nonnil (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
+  (hnnil : sprint_partial p ns start eaten rs ≠ []) → (sprint_partial p ns start eaten rs)[0]'(List.length_pos_iff.mpr hnnil) = rs := by
   intro hnnil
-  -- Use the trick of starting with 'step = step' followed by 'nth_rw'
+  -- Use the trick of starting with 'sprint_partial = sprint_partial' followed by 'nth_rw'
   -- to expand only the right-hand side. This allows us to easily do
   -- the necessary rewrites without having to worry about 'getElem_congr_coll'
-  have : step p ns start eaten rs = _ := rfl
-  nth_rw 2 [step] at this
+  have : sprint_partial p ns start eaten rs = _ := rfl
+  nth_rw 2 [sprint_partial] at this
   by_cases hterm : ns > 2 * p ∨ ¬close p start (loc rs)
   · -- If we hit the termination case this leads to a contradiction
     rw [dif_pos hterm] at this
@@ -104,71 +102,71 @@ lemma step_getElem_zero_of_nonnil (p ns : Nat) (start : Int × Int) (eaten : Lis
   rw [dif_neg hterm] at this
   rw [getElem_congr_coll this, List.getElem_cons_zero]
 
--- If the list constructed by 'step' is not empty, ns < 2 * p + 1
-lemma step_nslt_of_nonnil (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
-  (hnnil : step p ns start eaten rs ≠ []) → ns < 2 * p + 1 := by
+-- If the list constructed by 'sprint_partial' is not empty, ns < 2 * p + 1
+lemma sprint_partial_nslt_of_nonnil (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
+  (hnnil : sprint_partial p ns start eaten rs ≠ []) → ns < 2 * p + 1 := by
   intro hnnil
   apply Nat.lt_add_one_of_le
   contrapose! hnnil
   rename' hnnil => ltns
-  unfold step
+  unfold sprint_partial
   rw [dif_pos (Or.inl ltns)]
 
--- If the list constructed by 'step' is not empty, then the first element is close to 'start'
-lemma step_close_getElem_zero_of_nonnil (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
-  (hnnil : step p ns start eaten rs ≠ []) →
-  close p start (loc ((step p ns start eaten rs)[0]'(List.length_pos_iff.mpr hnnil))) := by
+-- If the list constructed by 'sprint_partial' is not empty, then the first element is close to 'start'
+lemma sprint_partial_close_getElem_zero_of_nonnil (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
+  (hnnil : sprint_partial p ns start eaten rs ≠ []) →
+  close p start (loc ((sprint_partial p ns start eaten rs)[0]'(List.length_pos_iff.mpr hnnil))) := by
   intro hnnil
-  rw [step_getElem_zero_of_nonnil]
+  rw [sprint_partial_getElem_zero_of_nonnil]
   · contrapose! hnnil
     rename' hnnil => hnclose
-    unfold step
+    unfold sprint_partial
     rw [dif_pos (Or.inr hnclose)]
   · assumption
 
--- If the list constructed by 'step' is not empty, rewrite it in 'cons' form.
-lemma step_cons_of_nonnil (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
-  (hnnil : step p ns start eaten rs ≠ []) →
-  step p ns start eaten rs = (rs :: (step p (ns + 1) start eaten (next_step eaten rs))) := by
+-- If the list constructed by 'sprint_partial' is not empty, rewrite it in 'cons' form.
+lemma sprint_partial_cons_of_nonnil (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
+  (hnnil : sprint_partial p ns start eaten rs ≠ []) →
+  sprint_partial p ns start eaten rs = (rs :: (sprint_partial p (ns + 1) start eaten (next_step eaten rs))) := by
   intro hnnil
-  nth_rw 1 [step]
+  nth_rw 1 [sprint_partial]
   split_ifs with hterm
   · apply hnnil
-    unfold step
+    unfold sprint_partial
     rw [dif_pos hterm]
   · rfl
 
-lemma step_mem_close (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
-  ∀ x ∈ step p ns start eaten rs, close p start (loc x) := by
+lemma sprint_partial_mem_close (p ns : Nat) (start : Int × Int) (eaten : List (Int × Int)) (rs : RunState) :
+  ∀ x ∈ sprint_partial p ns start eaten rs, close p start (loc x) := by
   intro x hxmem
   rcases List.getElem_of_mem hxmem with ⟨i, ilt, helem⟩
   by_cases iz : i = 0
-  · -- We've already proven that if 'step ≠ []', step[0] is "close"
+  · -- We've already proven that if 'sprint_partial ≠ []', sprint_partial[0] is "close"
     subst iz
     subst helem
-    apply step_close_getElem_zero_of_nonnil
+    apply sprint_partial_close_getElem_zero_of_nonnil
     exact List.ne_nil_of_mem hxmem
   · rename' iz => inz; push_neg at inz
     -- Otherwise, rewrite the list in 'cons' form and use recursion to close the goal
-    rw [getElem_congr_coll (step_cons_of_nonnil p ns start eaten rs (List.ne_nil_of_mem hxmem))] at helem
+    rw [getElem_congr_coll (sprint_partial_cons_of_nonnil p ns start eaten rs (List.ne_nil_of_mem hxmem))] at helem
     rw [getElem_congr_idx ((Nat.sub_one_add_one inz).symm), List.getElem_cons_succ] at helem
-    exact step_mem_close _ _ _ _ _ x (List.mem_of_getElem helem)
+    exact sprint_partial_mem_close _ _ _ _ _ x (List.mem_of_getElem helem)
 termination_by (2 * p + 1 - ns)
 decreasing_by
-  have hnnil : step p ns start eaten rs ≠ [] :=
+  have hnnil : sprint_partial p ns start eaten rs ≠ [] :=
     List.ne_nil_of_length_pos (lt_of_le_of_lt (Nat.zero_le _) ilt)
   have nsle : ns ≤ 2 * p := by
     apply Nat.le_of_lt_add_one
-    apply step_nslt_of_nonnil
+    apply sprint_partial_nslt_of_nonnil
     exact hnnil
   rw [add_comm ns, Nat.sub_add_eq, Nat.add_one_sub_one, Nat.sub_add_comm nsle]
   exact Nat.lt_add_one_of_le (le_refl _)
 
--- A sprint is a sequence of steps taken by the runner between moves by the devil
+-- A sprint is a sequence of steps taken by the runner between moves by the devil.
 -- Later we will show that the cells corresponding to the beginning and end of
 -- the sprints can be used to construct an angel which escapes the Nice Devil.
 def sprint (p : Nat) (rs₀ : RunState) (eaten : List (Int × Int)) : List RunState :=
-  step p 1 (loc rs₀) eaten rs₀
+  sprint_partial p 1 (loc rs₀) eaten rs₀
 
 -- If 'p' is positive, the sprint will not be empty
 -- Up to this point we have not needed this assumption, but it appears
@@ -176,7 +174,7 @@ def sprint (p : Nat) (rs₀ : RunState) (eaten : List (Int × Int)) : List RunSt
 lemma sprint_nonnil_of_ppos (p : Nat) (rs₀ : RunState) (eaten : List (Int × Int)) :
   0 < p → sprint p rs₀ eaten ≠ [] := by
   intro hpos
-  unfold sprint step
+  unfold sprint sprint_partial
   split_ifs with hterm
   · contrapose! hterm
     constructor
@@ -193,6 +191,6 @@ lemma sprint_close_first_last (p : Nat) (hpos : 0 < p) (rs₀ : RunState) (eaten
   (loc ((sprint p rs₀ eaten).getLast (sprint_nonnil_of_ppos p rs₀ eaten hpos))) := by
   have hnnil := sprint_nonnil_of_ppos p rs₀ eaten hpos
   unfold sprint at *
-  rw [List.head_eq_getElem_zero hnnil, step_getElem_zero_of_nonnil]
-  · exact step_mem_close p 1 (loc rs₀) eaten _ _ (List.getLast_mem hnnil)
+  rw [List.head_eq_getElem_zero hnnil, sprint_partial_getElem_zero_of_nonnil]
+  · exact sprint_partial_mem_close p 1 (loc rs₀) eaten _ _ (List.getLast_mem hnnil)
   · assumption
