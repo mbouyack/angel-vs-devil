1e0e5cb9ae1cceabda5fc4ec4a757b1aa73191f2
Matthew Bouyack <matthew@bouyack.tech>
2025-10-02 12:36:04 -0400

    Add 'Path' and misc region theorems
    A path is a list of orthogonally connected cells which form a subset
    of some other list of cells. This structure will be used to prove one
    of the defining features of a region: that there exists a path from
    every cell in the region to the starting cell.

--- AngelDevil/Region.lean
+++ AngelDevil/Region.lean
@@ -3,6 +3,25 @@ import AngelDevil.Dupes
 
 set_option linter.style.longLine false
 
+def up (a : Int × Int) := (a.1, a.2 + 1)
+def down (a : Int × Int) := (a.1, a.2 - 1)
+def right (a : Int × Int) := (a.1 + 1, a.2)
+def left (a : Int × Int) := (a.1 - 1, a.2)
+
+def adjacent (a b : Int × Int) : Prop :=
+  up a = b ∨ down a = b ∨ left a = b ∨ right a = b
+
+structure Path where
+  route : List (Int × Int)
+  cells : List (Int × Int)
+  hmem : route ⊆ cells
+  hadj : ∀ i (ilt : i < cells.length - 1),
+    adjacent (cells[i]'(
+      lt_of_lt_of_le ilt (Nat.sub_le _ _)
+    )) (cells[i+1]'(
+      Nat.add_lt_of_lt_sub ilt
+    ))
+
 -- NOTE: Ideally we would use 'Finset' in the RegionBuilder instead of
 -- 'List' with 'nodupe' assertions, but many of the operations and theorems
 -- we would need are either missing, awkward, or non-computable.
@@ -67,6 +86,10 @@ def region_builder_init (L : List (Int × Int)) (_start : (Int × Int)) : Region
       exact list_rm_dupes_nodupes L
     · apply list_nodupes_erase_of_nodupes
       exact list_rm_dupes_nodupes L
+
+-- 'start' in 'region_builder_init' is as given
+lemma region_builder_init_start (L : List (Int × Int)) (start : (Int × Int)) :
+  (region_builder_init L start).start = start := rfl
 
 -- Try moving a cell from 'unvisited' to 'pending'
 def region_builder_try_add_cell (RB : RegionBuilder) (c : Int × Int) : RegionBuilder :=
@@ -136,6 +159,24 @@ lemma region_builder_try_add_cell_length_invariant (RB : RegionBuilder) (c : Int
     rw [Nat.sub_one_add_one (Nat.ne_zero_of_lt (List.length_pos_of_mem h))]
   · rfl
 
+-- Any member of the partial region in 'RB' will still be present
+-- after calling region_builder_try_add_cell
+lemma region_builder_mem_try_add_cell_of_mem
+  (RB : RegionBuilder) (oldc newc : Int × Int) (oldcmem : oldc ∈ RB.region) :
+  oldc ∈ (region_builder_try_add_cell RB newc).region := by
+  unfold region_builder_try_add_cell
+  split_ifs with h
+  · dsimp; assumption
+  · assumption
+
+-- Calling 'region_builder_try_add_cell' has no effect on 'start'
+lemma region_builder_try_add_cell_start (RB : RegionBuilder) (c : Int × Int) :
+  (region_builder_try_add_cell RB c).start = RB.start := by
+  unfold region_builder_try_add_cell
+  split_ifs with h
+  · dsimp
+  · rfl
+
 def region_builder_add_pending_of_ne_nil (RB : RegionBuilder) (hnnil : RB.pending ≠ []) : RegionBuilder where
   start := RB.start
   region := (RB.pending.head hnnil)::RB.region
@@ -189,34 +230,135 @@ lemma region_builder_add_pending_of_ne_nil_length (RB : RegionBuilder) (hnnil : 
   rw [← Nat.sub_add_comm _]
   exact Nat.add_one_le_of_lt (List.length_pos_of_ne_nil hnnil)
 
+-- Any member of the partial region in 'RB' will still be present
+-- after calling region_builder_add_pending_of_ne_nil
+lemma region_builder_mem_add_pending_of_mem
+  (RB : RegionBuilder) (hnnil : RB.pending ≠ []) (c : Int × Int) (cmem : c ∈ RB.region) :
+  c ∈ (region_builder_add_pending_of_ne_nil RB hnnil).region := by
+  unfold region_builder_add_pending_of_ne_nil; dsimp
+  apply List.mem_cons_of_mem _ cmem
+
+lemma region_builder_add_pending_start (RB : RegionBuilder) (hnnil : RB.pending ≠ []) :
+  (region_builder_add_pending_of_ne_nil RB hnnil).start = RB.start := by
+  unfold region_builder_add_pending_of_ne_nil; dsimp
+
+-- Main processing step of the 'make_region' algorithm.
+-- This moves the head of the 'pending' list into the region and
+-- attempts to add the four orthogonally adjacent cells to the
+-- 'pending' list if they are in the 'unvisited' list.
+-- This is defined separately from 'make_region_impl' to make it
+-- easier to prove termination.
+def make_region_step (RB : RegionBuilder) (hnnil : RB.pending ≠ []) : RegionBuilder :=
+  (region_builder_add_pending_of_ne_nil (
+    region_builder_try_add_cell (
+    region_builder_try_add_cell (
+    region_builder_try_add_cell (
+    region_builder_try_add_cell RB
+      ((RB.pending.head hnnil).1 + 1, (RB.pending.head hnnil).2))
+      ((RB.pending.head hnnil).1 - 1, (RB.pending.head hnnil).2))
+      ((RB.pending.head hnnil).1, (RB.pending.head hnnil).2 + 1))
+      ((RB.pending.head hnnil).1, (RB.pending.head hnnil).2 - 1)) (by
+    apply region_builder_try_add_cell_pending_ne_nil
+    apply region_builder_try_add_cell_pending_ne_nil
+    apply region_builder_try_add_cell_pending_ne_nil
+    exact region_builder_try_add_cell_pending_ne_nil _ _ hnnil
+  ))
+
+-- Reusable termination proof for 'make_region' and related theorems
+lemma make_region_terminates (RB : RegionBuilder) (hnnil : RB.pending ≠ []) :
+  (make_region_step RB hnnil).pending.length +
+  (make_region_step RB hnnil).unvisited.length <
+  RB.pending.length + RB.unvisited.length := by
+  unfold make_region_step
+  rw [region_builder_add_pending_of_ne_nil_length]
+  repeat rw [region_builder_try_add_cell_length_invariant]
+  exact Nat.sub_one_lt (Nat.ne_zero_of_lt (Nat.add_pos_left (List.length_pos_of_ne_nil hnnil) _))
+
 -- Main loop of the region builder algorithm
 -- The algorithm terminates if there are no pending cells, otherwise
 -- it tries to add the four cells orthogonally connected to the current
 -- pending cell, adds the current pending cell to the region, and then
 -- recurses.
 def make_region_impl (RB : RegionBuilder) :=
-  if hnil : RB.pending = [] then RB else make_region_impl
-  (region_builder_add_pending_of_ne_nil (
-    region_builder_try_add_cell (
-    region_builder_try_add_cell (
-    region_builder_try_add_cell (
-    region_builder_try_add_cell RB
-      ((RB.pending.head hnil).1 + 1, (RB.pending.head hnil).2))
-      ((RB.pending.head hnil).1 - 1, (RB.pending.head hnil).2))
-      ((RB.pending.head hnil).1, (RB.pending.head hnil).2 + 1))
-      ((RB.pending.head hnil).1, (RB.pending.head hnil).2 - 1)) (by
-    apply region_builder_try_add_cell_pending_ne_nil
-    apply region_builder_try_add_cell_pending_ne_nil
-    apply region_builder_try_add_cell_pending_ne_nil
-    exact region_builder_try_add_cell_pending_ne_nil _ _ hnil
-  ))
+  if hnil : RB.pending = [] then RB else
+  make_region_impl (make_region_step RB hnil)
 termination_by RB.pending.length + RB.unvisited.length
 decreasing_by
-  rw [region_builder_add_pending_of_ne_nil_length]
-  repeat rw [region_builder_try_add_cell_length_invariant]
-  exact Nat.sub_one_lt (Nat.ne_zero_of_lt (Nat.add_pos_left (List.length_pos_of_ne_nil hnil) _))
+  exact make_region_terminates _ _
+
+-- The RegionBuilder returned by 'make_region_impl' has pending = []
+lemma make_region_impl_pending_nil (RB : RegionBuilder) :
+  (make_region_impl RB).pending = [] := by
+  unfold make_region_impl
+  split_ifs with h
+  · assumption
+  apply make_region_impl_pending_nil
+termination_by RB.pending.length + RB.unvisited.length
+decreasing_by
+  exact make_region_terminates _ _
+
+-- 'start' in 'make_region_impl' is as given
+lemma make_region_impl_start (RB : RegionBuilder) :
+  (make_region_impl RB).start = RB.start := by
+  unfold make_region_impl make_region_step
+  split_ifs with h
+  · rfl
+  rw [make_region_impl_start]
+  rw [region_builder_add_pending_start]
+  repeat rw [region_builder_try_add_cell_start]
+termination_by RB.pending.length + RB.unvisited.length
+decreasing_by
+  exact make_region_terminates _ _
+
+-- Any member of the partial region in 'RB' will still be present
+-- after calling make_region_impl
+lemma make_region_impl_mem_of_mem
+  (RB : RegionBuilder) (c : Int × Int) (cmem : c ∈ RB.region) :
+  c ∈ (make_region_impl RB).region := by
+  unfold make_region_impl
+  split_ifs with h
+  · assumption
+  push_neg at h
+  apply make_region_impl_mem_of_mem
+  apply region_builder_mem_add_pending_of_mem
+  repeat apply region_builder_mem_try_add_cell_of_mem
+  assumption
+termination_by RB.pending.length + RB.unvisited.length
+decreasing_by
+  exact make_region_terminates _ _
 
 -- Find the region of orthogonally connected cells in 'L' which contains 'start'
 -- Note that if start ∉ L it will return a 1-cell region with just 'start'.
 def make_region (L : List (Int × Int)) (start : (Int × Int)) : RegionBuilder :=
   make_region_impl (region_builder_init L start)
+
+-- 'start' in a 'make_region' is as given
+lemma make_region_start (L : List (Int × Int)) (start : (Int × Int)) :
+  (make_region L start).start = start := by
+  unfold make_region
+  rw [make_region_impl_start, region_builder_init_start]
+
+-- 'pending' in a 'make_region' is nil
+lemma make_region_pending_nil (L : List (Int × Int)) (start : (Int × Int)) :
+  (make_region L start).pending = [] := by
+  unfold make_region
+  exact make_region_impl_pending_nil _
+
+def Region (L : List (Int × Int)) (start : (Int × Int)) : List (Int × Int) :=
+  (make_region L start).region
+
+-- The starting cell is a member of the resulting region
+lemma region_start_mem (L : List (Int × Int)) (start : (Int × Int)) :
+  start ∈ Region L start := by
+  unfold Region
+  apply Or.elim (List.mem_append.mp (make_region L start).hstart)
+  · intro h
+    rwa [make_region_start] at h
+  · intro h
+    rw [make_region_pending_nil L start] at h
+    exact False.elim (List.not_mem_nil h)
+
+-- No region is empty (since it contains at least 'start')
+lemma region_ne_nil (L : List (Int × Int)) (start : (Int × Int)) :
+  Region L start ≠ [] :=
+  List.ne_nil_of_mem (region_start_mem L start)
